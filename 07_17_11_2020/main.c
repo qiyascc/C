58d6737b9bdf1f3024150737171c834981823320
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-11-19 22:20:49 +0400

    Update main.c

--- 07_17_11_2020/main.c
+++ 07_17_11_2020/main.c
@@ -1,19 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // Güvenlik uyarılarını devre dışı bırakmak için tanımlama
 #include <stdio.h>
 
-int max(int a, int b)
-{
-	return a > b ? a : b;
+// İki sayı arasındaki maksimumu döndüren fonksiyon
+int max(int a, int b) {
+    return a > b ? a : b; // Koşullu ifade (ternary) operatörü kullanımı
 }
 
+// Ana fonksiyon
+int main() {
+    int x, y, z, t; // Kullanıcıdan alınacak dört tam sayı
 
-int main()
-{
-	int x, y, z, t;
-	printf("4 sayi giriniz\n");
-	(void)scanf("%d%d%d%d", &x, &y, &z, &t);
-	
-	int ival = max(max(x, y),max(z,t));
-	printf("ival = %d\n",ival);
-	printf("test\n");
+    // Kullanıcıdan dört sayı girmesini isteyin
+    printf("4 sayi giriniz\n");
+    // Girilen sayıları değişkenlere atayın ve hata denetimi için (void) kullanın
+    (void)scanf("%d%d%d%d", &x, &y, &z, &t);
+
+    // Girilen dört sayıdan maksimum olanını bulun
+    int ival = max(max(x, y), max(z, t));
+    // Maksimum sayıyı yazdırın
+    printf("En buyuk sayi = %d\n", ival);
+
+    // Test amaçlı ek bir mesaj yazdırın
+    printf("test\n");
+
+    return 0; // Programın başarılı şekilde sonlandığını belirtin
 }
