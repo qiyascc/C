d48086daad45a2b5fbf983e14e05a50ab59e5304
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-11-19 22:19:54 +0400

    Update main.c

--- 06_12_11_2020/main.c
+++ 06_12_11_2020/main.c
@@ -1,61 +1,60 @@
 #include <stdio.h>
 
-int sum(int x, int y)
-{
-	return x + y;
+// Toplama işlemi yapan fonksiyon
+int sum(int x, int y) {
+    return x + y;
 }
 
-int max(int a, int b, int c)
-{
-	int max = a;
+// Üç sayı arasından en büyüğünü döndüren fonksiyon
+int max(int a, int b, int c) {
+    int max_val = a;  // En büyük değeri saklamak için
 
-	if (b > a)
-	{
-		max = b;
-	}
+    if (b > max_val) {
+        max_val = b;
+    }
 
-	if (c > max)
-	{
-		max = c;
-	}
-	
-	return max;
+    if (c > max_val) {
+        max_val = c;
+    }
+
+    return max_val;
 }
 
-int abs(int a)
-{
-	if (a < -1)
-	{
-		return -a;
-	}
-	
-	return a;
+// Mutlak değer döndüren fonksiyon
+int abs(int a) {
+    if (a < 0) {  // Negatif kontrolü
+        return -a;
+    }
+
+    return a;
 }
 
-int ndigit(int x)
-{
-	int digit_count = 0;
+// Bir sayının kaç basamaklı olduğunu hesaplayan fonksiyon
+int ndigit(int x) {
+    int digit_count = 0;  // Basamak sayısını tutacak değişken
 
-	if (x == 0)
-		return 1;
+    // Sıfır kontrolü
+    if (x == 0) {
+        return 1;
+    }
 
-	while (x != 0)
-	{
-		digit_count++;
-		x /= 10;
-	}
+    // Basamakları say
+    while (x != 0) {
+        digit_count++;
+        x /= 10;
+    }
 
-	return digit_count;
-
+    return digit_count;
 }
 
+// Ana fonksiyon
+int main() {
+    // Fonksiyonların test edilmesi
+    printf("%d\n", sum(5,6));        // 5 + 6 = 11
+    printf("%d\n", max(5, 6, 9));    // 5, 6, 9 arasında en büyük 9
+    printf("%d\n", abs(-2));         // |-2| = 2
+    printf("%d\n", abs(3));          // |3| = 3
+    printf("%d", ndigit(2579));      // 2579 kaç basamaklı? Cevap: 4
 
-int main()
-{
-	printf("%d\n", sum(5,6));
-	printf("%d\n", max(5, 6, 9));
-	printf("%d\n", abs(-2));
-	printf("%d\n", abs(3));
-	printf("%d", ndigit(2579));
-   	
+    return 0;
 }
